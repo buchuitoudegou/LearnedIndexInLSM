adf9d440e68e14b44453362adad51d4c9c7b5c9b
buchuitoudegou <756541536@qq.com>
2024-09-05 23:47:57 +0800

    remove unused code

--- util/env_posix.cc
+++ util/env_posix.cc
@@ -818,11 +818,11 @@ class PosixEnv : public Env {
 
         // if the file has been created for longer than wait_time (learn_trigger_time),
         // we can do CBA analysis. else, wait until the file has lived wait_time
-        time_diff = front.first + adgMod::learn_trigger_time - time_start;
-        if (time_diff > 0) {
-          wait_for_time = true;
-          break;
-        }
+        // time_diff = front.first + adgMod::learn_trigger_time - time_start;
+        // if (time_diff > 0) {
+        //   wait_for_time = true;
+        //   break;
+        // }
 
         learning_prepare.pop();
         double score = adgMod::learn_cb_model->CalculateCB(level, front.second.second->file_size);
@@ -844,15 +844,6 @@ class PosixEnv : public Env {
         adgMod::filelearn_count++;
         learn_pq.pop();
       }
-
-      // if we decide to wait, sleep here
-      if (wait_for_time) {
-        // prepare_queue_mutex.Unlock();
-        SleepForMicroseconds((int) (time_diff / 1000));
-        // prepare_queue_mutex.Lock();
-        wait_for_time = false;
-      }
-    // }
   }
 
   static void PrepareLearnEntryPoint(PosixEnv* env) {
@@ -868,8 +859,7 @@ class PosixEnv : public Env {
         // background_thread.detach();
         
     // }
-    PrepareLearnEntryPoint(this);   
-    // if (learning_prepare.empty()) preparing_queue_cv.Signal();
+    PrepareLearnEntryPoint(this);
     learning_prepare.emplace(std::make_pair(time_start, std::make_pair(level, meta)));
     adgMod::newSST_count++;
   }
